ff8a68ae626bd8b4c74b7f336ed1cb083d0c577a
TANZHIJIAN14 <jiantanzhi@gmail.com>
2024-12-11 01:46:07 +0800

    Update kafka script

--- kafka-topics.sh
+++ kafka-topics.sh
@@ -3,11 +3,11 @@
 echo "Setting up environment..."
 
 # Create topic: UploadedFile
-kafka-topics.sh --create --topic UploadedFile --bootstrap-server localhost:9092
+kafka-topics.sh --create --topic UploadedFile --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
 echo "Topic 'UploadedFile' created."
 
 # Create topic: DeletedFile
-kafka-topics.sh --create --topic DeletedFile --bootstrap-server localhost:9092
+kafka-topics.sh --create --topic DeletedFile --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
 echo "Topic 'DeletedFile' created."
 
 echo "Environment setup complete!"
